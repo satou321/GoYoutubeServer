68b92a456782bc068ebc547af0c1eff640f7cc9b
satou321 <iitsukanan@yahoo.co.jp>
2019-07-29 15:39:01 +0900

    test

--- server/controllers/webserver.go
+++ server/controllers/webserver.go
@@ -30,5 +30,13 @@ func StartWebServer(port string) error {
 	//addr := fmt.Sprintf(":%d", port)
 	//addr := ":" + port
 	log.Println("[info] http server listening", port)
+	//return http.ListenAndServe(":"+port, nil)
+	//	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+	print(port)
+
+	//http.ListenAndServe(":"+port, Log(router))
 	return http.ListenAndServe(":"+port, nil)
 }
