1dcca1c4e0de31a893abb16fcafb66195e59e0d9
satou321 <iitsukanan@yahoo.co.jp>
2019-07-29 13:22:30 +0900

    fix

--- main.go
+++ main.go
@@ -6,12 +6,6 @@ import (
 	"os"
 )
 
-func init() {
-	//for local. open browser after you build
-	//port := strconv.Itoa(config.Config.Port)
-	//log.Print(open.Run("http://localhost:" + port + "/r"))
-}
-
 func main() {
 	//var cfg Config
 	//cfg := config.Config
