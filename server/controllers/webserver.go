acfbcb679342d8c447cc3b5774688b51cd9f2de4
satou321 <iitsukanan@yahoo.co.jp>
2019-07-29 15:13:57 +0900

    test

--- server/controllers/webserver.go
+++ server/controllers/webserver.go
@@ -28,7 +28,8 @@ func APIError(w http.ResponseWriter, errMessage string, code int) {
 }
 func StartWebServer(port string) error {
 	fmt.Println(port)
-	addr := fmt.Sprintf(":%s", port)
+	//addr := fmt.Sprintf(":%s", port)
+	addr := ":" + port
 	log.Println("[info] http server listening", addr)
 	return http.ListenAndServe(addr, nil)
 }
